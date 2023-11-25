151155b6423c5dce52d09bdf03ce6ef53043dd1f
Rem7474 <recuv007@hotmail.fr>
2023-11-25 17:42:37 +0100

    test

--- test1.php
+++ test1.php
@@ -6,13 +6,16 @@ include 'FonctionsConnexion.php';
 //connexion à la base de données
 $connex=connexionBDD('./private/parametres.ini');
 //test de la fonction ajouterUtilisateur
+print "après co :)<br />";
 $result=ajouterUtilisateur('Doe', 'John', '999618078', $connex);
 print_r($result);
+print "après ajout :)<br />";
 //test de la fonction getUtilisateur
 $result = getUtilisateur('999618078', $connex);
+print "après get :)<br />";
 print_r($result);
 echo $result['utilisateur_nom'];
 echo $result['utilisateur_prenom'];
 echo $result['utilisateur_rfid_uid'];
-echo "Bonjour";
+print "FIN :)<br />";
 ?>
