80465e5427654f476708338a743181aaf7a576fc
Rem7474 <recuv007@hotmail.fr>
2023-11-25 18:02:41 +0100

    test

--- test1.php
+++ test1.php
@@ -2,6 +2,15 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: application/json');
+$id=$_GET['id'];
+if (empty($id)) {
+    //affiche un message d'erreur formaté en JSON
+    die(json_encode(array('message' => 'Missing id')));
+}
+else if (!is_numeric($id)) {
+    //affiche un message d'erreur formaté en JSON
+    die(json_encode(array('message' => 'id must be numeric')));
+}
 //inclure le fichier fonctionsBDD
 include 'FonctionsBDD.php';
 //inclure le fichier fonctionsConnexion
@@ -12,7 +21,7 @@ $connex=connexionBDD('./private/parametres.ini');
 //$result=ajouterUtilisateur('Doe', 'John', '999618078', $connex);
 //print_r($result);
 //test de la fonction getUtilisateur
-$result = getUtilisateur('999618078', $connex);
+$result = getUtilisateur($id, $connex);
 $json = json_encode($result);
 echo $json;
 ?>
