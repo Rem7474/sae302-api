ac419ed9a0c7ae7835ed0c7d78704816516212bb
Rem7474 <recuv007@hotmail.fr>
2023-11-25 17:37:43 +0100

    test

--- FonctionsBDD.php
+++ FonctionsBDD.php
@@ -5,7 +5,7 @@ function getUtilisateur($id, $connex){
     $stmt = $connex->prepare($sql);
     $stmt->bindValue(':id', $id);
     $stmt->execute();
-    $result = $stmt->fetchALL();
+    $result = $stmt->fetchColumn();
     return $result;
 }
 //fonction test qui ajoute un utilisateur
