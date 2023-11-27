5d9743a125b3f420a99778cd2ae79fb4c1625914
RemEx <74838147+Rem7474@users.noreply.github.com>
2023-11-27 10:00:06 +0100

    add stock

--- addStock.php
+++ addStock.php
@@ -13,12 +13,10 @@ else if (!is_numeric($_GET['barecode']) || !is_numeric($_GET['stock'])) {
     die(json_encode(array('message' => 'Information must be numeric')));
 }
 $barecode = $_GET['barecode'];
-$nom = $_GET['nom'];
-$prixachat = $_GET['prixachat'];
-$prixvente = $_GET['prixvente'];
+$stock = $_GET['stock'];
 //connexion à la base de données
 $connex=connexionBDD('./private/parametres.ini');
-$result = ajoutProduit($barecode, $nom, $prixachat, $prixvente, $connex);
+$result = addStock($barecode, $stock, $connex);
 if ($result != null) {
     //affiche un message de succès formaté en JSON
     die(json_encode(array('message' => 'Stock added')));
